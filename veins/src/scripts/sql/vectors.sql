a508b54276f57266f2cb8b551137ba33d32fe4f4
Julian Timpner <timpner@ibr.cs.tu-bs.de>
2015-09-29 18:28:41 +0200

    Add SQL statements and code for hooking up omnetpp with direct MySQL logging

---
+++ veins/src/scripts/sql/vectors.sql
@@ -0,0 +1,35 @@
+#
+# Create tables for cMySQLOutputVectorManager.
+#
+
+DROP TABLE IF EXISTS vecdata;
+DROP TABLE IF EXISTS vecattr;
+DROP TABLE IF EXISTS vector;
+DROP TABLE IF EXISTS vrun;
+
+CREATE TABLE vrun (
+     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+     runnumber INT NOT NULL,
+     network VARCHAR(80) NOT NULL,
+     date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE vector (
+     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+     runid INT NOT NULL REFERENCES vrun(id),
+     module VARCHAR(200) NOT NULL,
+     name VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE vecattr (
+     vectorid INT NOT NULL REFERENCES vector(id),
+     name VARCHAR(200) NOT NULL,
+     value VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE vecdata (
+     vectorid INT NOT NULL REFERENCES vector(id),
+     time DOUBLE PRECISION NOT NULL,
+     value DOUBLE PRECISION NOT NULL
+) ENGINE = MYISAM;
+
