51f572779b7d30e0255e498645254463e6796e68
Julian Timpner <timpner@ibr.cs.tu-bs.de>
2015-10-05 16:56:39 +0200

    Clean up

--- veins/src/artery/application/VehicleTelemetryService.cc
+++ veins/src/artery/application/VehicleTelemetryService.cc
@@ -85,8 +85,7 @@ void VehicleTelemetryService::trigger()
 	        std::string roadId = getFacilities().getMobility().getVehicleCommandInterface()->getRoadId();
 	        int32_t laneIndex = getFacilities().getMobility().getVehicleCommandInterface()->getLaneIndex();
 	        double lanePosition = getFacilities().getMobility().getVehicleCommandInterface()->getLanePosition();
-	        double xPos = getFacilities().getMobility().getCurrentPosition().x;
-	        double yPos = getFacilities().getMobility().getCurrentPosition().y;
+	        Coord pos = getFacilities().getMobility().getVehicleCommandInterface()->getPosition();
 	        double speed = getFacilities().getMobility().getSpeed();
 	        std::string vehicleType = getFacilities().getMobility().getVehicleCommandInterface()->getTypeId();
 	        double vehicleLength = getFacilities().getMobility().getCommandInterface()->vehicletype(vehicleType).getLength();
@@ -96,8 +95,8 @@ void VehicleTelemetryService::trigger()
 	        report->setSrc(sumoId.c_str());
 	        report->setRoadId(roadId.c_str());
 	        report->setLaneIndex(laneIndex);
-	        report->setXPosition(xPos);
-	        report->setYPosition(yPos);
+	        report->setXPosition(pos.x);
+	        report->setYPosition(pos.y);
 	        report->setLanePosition(lanePosition);
 	        report->setSpeed(speed);
 	        report->setVehicleType(vehicleType.c_str());
